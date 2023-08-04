f17a37d5a7a5760fc6e7ec5a56a27b90a49fe278
Anna Krasilnikova <akr6363@mail.ru>
2023-08-04 21:52:55 +0300

    splitting components into different files

--- src/components/auth/personal-info/personal-info.tsx
+++ src/components/auth/personal-info/personal-info.tsx
@@ -1,16 +1,13 @@
-import { ChangeEvent, FC, useRef, useState } from 'react'
-
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { ChangeEvent, FC, useState } from 'react'
 
 import { Button } from '../../ui/button'
 
 import s from './personal-info.module.scss'
 
-import { Edit, LogOut, Photo } from '@/assets/icons/components'
+import { Edit, LogOut } from '@/assets/icons/components'
+import { EditForm } from '@/components/auth/personal-info/edit-form/edit-form.tsx'
+import { UploadingImagesBtn } from '@/components/auth/personal-info/uploading-img-btn/uploading-img-btn.tsx'
 import { Card, Typography } from '@/components/ui'
-import { ControlledTextField } from '@/components/ui/controlled/controlled-text-field/controlled-text-field.tsx'
 
 export type PersonalInfoProps = {
   email: string
@@ -75,74 +72,5 @@ export const PersonalInfo: FC<PersonalInfoProps> = ({
         <EditForm onSave={onNameChangeHandler} defaultValue={name} />
       )}
     </Card>
-  )
-}
-
-// ---------------------------------------------
-
-type UploadingImagesBtnPropsType = {
-  onChange(e: ChangeEvent<HTMLInputElement>): void
-}
-
-export const UploadingImagesBtn: React.FC<UploadingImagesBtnPropsType> = ({ onChange }) => {
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const handleClick = () => {
-    if (inputRef.current) {
-      inputRef.current.click()
-    }
-  }
-
-  return (
-    <>
-      <input type="file" onChange={onChange} style={{ display: 'none' }} ref={inputRef} />
-      <Button
-        variant={'secondary'}
-        onClick={handleClick}
-        icon={<Photo size={16} />}
-        className={s.editPhotoBtn}
-      />
-    </>
-  )
-}
-
-// ---------------------------------------------
-
-const logoutSchema = z.object({
-  name: z.string().nonempty('The field is required').min(3),
-})
-
-type FormValues = z.infer<typeof logoutSchema>
-export type EditFormProps = {
-  onSave: (name: string) => void
-  defaultValue: string
-}
-const EditForm: FC<EditFormProps> = ({ onSave, defaultValue }) => {
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<FormValues>({
-    resolver: zodResolver(logoutSchema),
-    mode: 'onSubmit',
-    defaultValues: {
-      name: defaultValue,
-    },
-  })
-  const onSubmit = (data: FormValues) => {
-    onSave(data.name)
-  }
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
-      <ControlledTextField
-        control={control}
-        name={'name'}
-        label={'Nickname'}
-        errorMessage={errors.name?.message}
-        className={s.inputName}
-      />
-      <Button type="submit">Save Changes</Button>
-    </form>
   )
 }
