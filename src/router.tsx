6aa42622e45b6bfc3387e3b4c828421d1c3fe2fd
Anna Krasilnikova <akr6363@mail.ru>
2023-08-16 15:44:38 +0300

    -authorization flow

--- src/router.tsx
+++ src/router.tsx
@@ -2,15 +2,16 @@ import { useEffect } from 'react'
 
 import {
   createBrowserRouter,
+  Navigate,
+  Outlet,
   RouteObject,
   RouterProvider,
-  Outlet,
-  Navigate,
 } from 'react-router-dom'
 
 import { Header } from '@/components/ui'
 import { Cards } from '@/pages/cards/cards.tsx'
 import { Decks } from '@/pages/decks/decks.tsx'
+import SignInPage from '@/pages/sign-in/sign-in.tsx'
 import { useAuthMeQuery } from '@/services/auth/auth-api.ts'
 import { authSlice } from '@/services/auth/auth-slice.ts'
 import { useAppDispatch } from '@/services/store.ts'
@@ -18,7 +19,7 @@ import { useAppDispatch } from '@/services/store.ts'
 const publicRoutes: RouteObject[] = [
   {
     path: '/login',
-    element: <div>login</div>,
+    element: <SignInPage />,
   },
 ]
 
@@ -55,20 +56,21 @@ const router = createBrowserRouter([
 ])
 
 export const Router = () => {
+  return <RouterProvider router={router} />
+}
+
+function PrivateRoutes() {
   const dispatch = useAppDispatch()
-  const { data } = useAuthMeQuery({})
+  const { data, isLoading } = useAuthMeQuery({})
 
   useEffect(() => {
     if (data) {
       dispatch(authSlice.actions.setAuthData(data))
     }
   }, [data])
+  if (isLoading) return <div>Loading...</div>
 
-  return <RouterProvider router={router} />
-}
-
-function PrivateRoutes() {
-  const isAuthenticated = true
+  const isAuthenticated = !!data
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 }
--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -1,14 +1,32 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi } from '@reduxjs/toolkit/query/react'
+
+import { customFetchBase } from '@/services/base-api-with-refetch.ts'
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  tagTypes: ['Decks', 'DecksUpdate', 'DecksDelete', 'CreateCard', 'EditCard', 'DeleteCard', 'Deck'],
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.flashcards.andrii.es',
-    credentials: 'include',
-    prepareHeaders: headers => {
-      headers.append('x-auth-skip', 'true')
-    },
-  }),
+  tagTypes: [
+    'Decks',
+    'DecksUpdate',
+    'DecksDelete',
+    'CreateCard',
+    'EditCard',
+    'DeleteCard',
+    'Deck',
+    'Me',
+  ],
+  baseQuery: customFetchBase,
   endpoints: () => ({}),
 })
+
+// export const baseApi = createApi({
+//   reducerPath: 'baseApi',
+//   tagTypes: ['Decks', 'DecksUpdate', 'DecksDelete', 'CreateCard', 'EditCard', 'DeleteCard', 'Deck'],
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: 'https://api.flashcards.andrii.es',
+//     credentials: 'include',
+//     prepareHeaders: headers => {
+//       // headers.append('x-auth-skip', 'true')
+//     },
+//   }),
+//   endpoints: () => ({}),
+// })
